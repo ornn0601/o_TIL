0e7300112022f19a9b6c9396035dabac085c894e
ornn <info@ornn.co.kr>
2023-01-10 16:28:17 +0900

    스크롤UI & 3D MODEL EFFECT

---
+++ inflearn_scroll-interaction/2. lesson/ggang_lesson/section10/study-2/main.js
@@ -0,0 +1,126 @@
+$(function() {
+
+  let winScrollTop;
+
+  let section = $('.ggang_section');
+  let offsetTop = [];
+  let offsetBottom = [];
+  
+  let currentIndex = -1; 
+
+  function setValue() {
+    winScrollTop = $(window).scrollTop();
+
+    section.each(function(index, obj) {
+      offsetTop[index] = $(obj).offset().top;
+      offsetBottom[index] = $(obj).offset().top + $(obj).height();
+    });
+  }
+
+  function checkInSection() {
+    if(winScrollTop >= offsetTop[0] && offsetBottom[0] > winScrollTop) {
+      sectionActive(0);
+    } else if(winScrollTop >= offsetTop[1] && offsetBottom[1] > winScrollTop) {
+      sectionActive(1)
+    } else if(winScrollTop >= offsetTop[2] && offsetBottom[2] > winScrollTop) {
+      sectionActive(2)
+    } else if(winScrollTop >= offsetTop[3] && offsetBottom[3] > winScrollTop) {
+      sectionActive(3)
+    } else if(winScrollTop >= offsetTop[4] && offsetBottom[4] > winScrollTop) {
+      sectionActive(4)
+    } else if(winScrollTop >= offsetTop[5] && offsetBottom[5] > winScrollTop) {
+      sectionActive(5)
+    } else if(winScrollTop >= offsetTop[6] && offsetBottom[6] > winScrollTop) {
+      sectionActive(6)
+    } else if(winScrollTop >= offsetTop[7] && offsetBottom[7] > winScrollTop) {
+      sectionActive(7)
+    }
+  }
+
+  function sectionActive(index) {
+
+    if(index !== currentIndex) {
+      listActive(index);
+      changeText(index);
+      changeColor(index);
+      changeImage(index);
+      console.log('in');
+    }
+
+    // 계속 호출 함수
+    parallax();
+
+    currentIndex = index;
+  }
+
+  function changeImage(index) {
+
+    $('.cube_box .cube_face').removeClass('s0 s1 s2 s3 s4 s5 s6 s7');
+    $('.cube_box .cube_face').addClass('s' + index);
+  }
+
+  function listActive(index) {
+    let list = $('.nav_list li a');
+
+    list.removeClass('active');
+    list.eq(index).addClass('active');
+  }
+
+  function changeText(index) {
+    let targetText = $('.fix_tit strong');
+    let list = $('.nav_list li a span');
+    let getText = list.eq(index).text();
+
+    targetText.text(getText);
+  }
+
+  function changeColor(index) {
+    let targetText = $('.fix_tit');
+
+    if(index === 1 || index === 2) {
+      targetText.addClass('black');
+    } else {
+      targetText.removeClass('black');
+    }
+  }
+
+  // cube
+  function parallax() {
+    let scrollHeight = $(document).height();
+    let scrollRealHeight = scrollHeight - $(window).height();
+    let scrollPercent = winScrollTop / scrollRealHeight;
+    let parallaxDistance = 1100; // 회전값
+    let moveDistance = parallaxDistance * scrollPercent; // 이동할 값
+
+    $('.cube_box').css({
+      transform : 'rotateY('+ moveDistance +'deg) rotateX('+ moveDistance +'deg)'
+		});
+  } 
+
+
+  $('.nav_list li a').click(function() {
+    if ($(this.hash).offset()) {
+      $('html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 500);
+    }
+  });
+
+  function init()  {
+    setValue();
+    checkInSection();
+  }
+
+  $(window).scroll(function() {
+    winScrollTop = $(window).scrollTop();
+    checkInSection();
+  });
+
+  $(window).resize(function() {
+    setValue();
+    checkInSection();
+  });
+
+  init(); // start
+
+});
